1d88560c1b0665639fd63e33cc2b039b98eecfca
Tiago Neves <95252546+tiagonevestec@users.noreply.github.com>
2021-11-30 02:20:36 -0300

    Add files via upload
    Aqui estão os arquivos do exercício, sinta-se livre para usar o código e avaliar...
    Obrigado pela colaboração e por passar por aqui.  :D

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Eu quero ser um aluno nota: %d \n", 1 + 9);
+    printf("Maria tem 40 anos e Joao tem 52, a soma das idades eh: %d \n", 40 + 52);
+    printf("Marcelo comprou 5 tomates, cada um custou R$2.30, ele gastou: R$%1.2f \n", 5 * 2.30);
+    printf("Joana comprou 23 caixas de leite e gastou R$57.50. Cada caixa custou: R$%1.2f \n", 57.50 / 23);
+
+    return 0;
+}
